b453914ae9d00bd4ba95b6441c1d8c88ee102a26
Felipe <FelipeCll01@outlook.com>
2022-08-04 09:29:05 -0300

    Adicionando animação com scroll

---
+++ js/scroll.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal ({reset: true});
+ScrollReveal({distance: '30px'});
+
+sr.reveal('.descricao', {duration: 2500, origin: 'left'})
+sr.reveal('.principal-botao', {duration: 2500, origin: 'left'})
+sr.reveal('.desenho-sobre', {duration: 3000, origin: 'left'})
+sr.reveal('.titulo-sobre', {duration: 3000, origin: 'top'})
+sr.reveal('.texto-box', {duration: 3000, origin: 'right'})
+sr.reveal('.project', {duration: 3000, origin: 'top'})
+sr.reveal('.projetos-itens', {duration: 3000, origin: 'bottom'})
+sr.reveal('.conteiner-skills .skill-box', {duration: 3000, origin: 'left', interval: 200})

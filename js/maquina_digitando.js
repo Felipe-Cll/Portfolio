994b21b785f926d3ee68c1db07dfeb6a976613a6
Felipe <FelipeCll01@outlook.com>
2022-08-04 08:08:52 -0300

    Alteração dos projetos e footer

--- js/maquina_digitando.js
+++ js/maquina_digitando.js
@@ -1,5 +1,7 @@
 const meuNome = document.querySelector('.meu-nome');
 const maqName = meuNome.innerHTML;
+const botaoVerMais = document.querySelector('button');
+const botaoProjetos = document.querySelectorAll('.escondido');
 meuNome.innerHTML='';
 
 function typeWriter(nome) {
@@ -13,3 +15,23 @@ function typeWriter(nome) {
             }, 150 * index)
         })}, 1500)
 }
+
+function cliqueVer (e) {
+    botaoProjetos.forEach((elemento) => {
+        if (elemento.classList.contains('escondido')) {
+            elemento.classList.remove('escondido');
+            elemento.classList.add('visivel')
+            botaoVerMais.innerHTML = 'Ver mais';
+        } else {
+            elemento.classList.add('escondido');
+            elemento.classList.remove('visivel');
+            botaoVerMais.innerHTML = 'Ver mais';
+        }
+    })
+}
+
+function maisProjetos (elemento) {
+    elemento.addEventListener('click', cliqueVer)
+}
+
+maisProjetos(botaoVerMais);

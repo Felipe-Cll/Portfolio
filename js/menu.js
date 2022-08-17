7d3b43c774e1c5b784011ce03ae5cc6eee461bc5
Felipe <FelipeCll01@outlook.com>
2022-08-17 15:39:32 -0300

    Adicionando menu mobile

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+$(".menu_btn").click(function () {
+    $(".navbar_menu").toggleClass("active");
+    $(".menu_btn i").toggleClass("active");
+});
+
+$(".navbar_menu li a").click(function () {
+    $(".navbar_menu").toggleClass("active");
+    $(".menu_btn i").toggleClass("active");
+});

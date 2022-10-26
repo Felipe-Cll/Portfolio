f27ed00f8649971298fc550419f5d23cd3effc80
Felipe <FelipeCll01@outlook.com>
2022-10-26 14:46:24 -0300

    maintenance

--- js/menu.js
+++
@@ -1,9 +0,0 @@
-$(".menu_btn").click(function () {
-    $(".navbar_menu").toggleClass("active");
-    $(".menu_btn i").toggleClass("active");
-});
-
-$(".navbar_menu li a").click(function () {
-    $(".navbar_menu").toggleClass("active");
-    $(".menu_btn i").toggleClass("active");
-});
